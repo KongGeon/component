40b2f21acd82144883c45d38b5c8854643101d1c
SnagGeon <yangsk0526@naver.com>
2024-01-08 18:52:43 +0900

    header 내용 추가

--- Components/01_01_header_pc/header01/js/main.js
+++ Components/01_01_header_pc/header01/js/main.js
@@ -1,6 +1,5 @@
 //pc메뉴
 let pcMenuBtn = document.querySelectorAll(".pc-menu__step1");
-let pcMenuBtnList = document.querySelectorAll(".pc-menu__step1 + ul");
 
 for (let i = 0; i < pcMenuBtn.length; i++) {
   const p = pcMenuBtn[i];
@@ -13,12 +12,12 @@ for (let i = 0; i < pcMenuBtn.length; i++) {
 }
 let preMenu = document.querySelector(".s__logo"); //pc메뉴의 이전요소
 let nextMenu = document.querySelector(".s__login"); //pc메뉴의 다음요소
-preMenu.addEventListener("focusin", () => {
+preMenu&&preMenu.addEventListener("focusin", () => {
   pcMenuBtn.forEach((e) => {
     e.classList.remove("menu-on");
   });
 });
-nextMenu.addEventListener("focusin", () => {
+nextMenu&&nextMenu.addEventListener("focusin", () => {
   pcMenuBtn.forEach((e) => {
     e.classList.remove("menu-on");
   });
